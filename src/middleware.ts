060cac511bb7e4cea3b42f9daf3a0ee55f105b88
GUILLERMO RECALDE <recaldev.ga@gmail.com>
2025-10-06 10:53:23 -0300

    Exclude static files (e.g. /polipizza.png) from auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -46,8 +46,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - any file with an extension (serve from /public without auth)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)',
   ],
 }
 
